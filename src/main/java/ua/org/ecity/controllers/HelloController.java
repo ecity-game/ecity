522982f084963592361bae55fe1f3cabc8114537
Dima D <dergachovda@gmail.com>
2016-11-09 12:17:33 +0200

    add Name entity and controller /names

--- src/main/java/ua/org/ecity/controllers/HelloController.java
+++ src/main/java/ua/org/ecity/controllers/HelloController.java
@@ -3,8 +3,10 @@ package ua.org.ecity.controllers;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 import ua.org.ecity.entities.City;
+import ua.org.ecity.entities.Name;
 import ua.org.ecity.services.CityService;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @RestController
@@ -26,8 +28,21 @@ public class HelloController {
     }
 
     @RequestMapping("/cities")
-    public @ResponseBody List<City> city() {
+    public @ResponseBody List<City> cities() {
         return cityService.getCities();
+    }
+
+    @RequestMapping("/names")
+    public @ResponseBody List<Name> names() {
+        List<City> cities = cityService.getCities();
+        List<Name> names = new ArrayList<>();
+        for (City city : cities) {
+            Name name = new Name();
+            name.setId(city.getId());
+            name.setName(city.getName());
+            names.add(name);
+        }
+        return names;
     }
 
     @RequestMapping("/user/hello")
