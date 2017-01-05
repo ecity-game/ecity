0344ea773b6c297e744a231f840b6fd06239717e
Dima D <dergachovda@gmail.com>
2017-01-05 13:23:45 +0200

    add lastCharTest

---
+++ src/test/java/ua/org/ecity/entities/CityTest.java
@@ -0,0 +1,38 @@
+package ua.org.ecity.entities;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+
+@RunWith(JUnit4.class)
+public class CityTest {
+
+    City city;
+
+    @Before
+    public void setUp() {
+        city = new City();
+    }
+
+    @Test
+    public void lastCharTest() {
+        city.setName("Черновцы");
+        assertThat(city.getLastChar(), is('В'));
+
+        city.setName("Ы");
+        assertThat(city.getLastChar(), is('Ы'));
+
+        city.setName("АбвЙЫЬЪЦ");
+        assertThat(city.getLastChar(), is('В'));
+
+        city.setName("Ирпень");
+        assertThat(city.getLastChar(), is('Н'));
+
+        city.setName("Одесса");
+        assertThat(city.getLastChar(), is('А'));
+    }
+}
