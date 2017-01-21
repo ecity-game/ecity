f2800e6f6080ad93e49733393d7ed2f36dfa5128
Dima Derhachov <dima.derhachov@gmail.com>
2017-01-21 18:47:05 +0200

    update front

--- src/main/java/ua/org/ecity/controllers/LoginController.java
+++ src/main/java/ua/org/ecity/controllers/LoginController.java
@@ -1,13 +1,8 @@
 package ua.org.ecity.controllers;
 
-import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 import ua.org.ecity.entities.Result;
-
-import javax.servlet.http.HttpSession;
 
 @RestController
 public class LoginController {
